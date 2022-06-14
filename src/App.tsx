50e3ad440927f20ba3bd3f8f5ee8cf1372f84107
Jonathon Armstrong <jonathon.armstrong@varsitytutors.com>
2022-06-14 14:02:32 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+
+//
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Dashboardl
+      </header>
+    </div>
+  );
+}
+
+//
+
+export default App;
